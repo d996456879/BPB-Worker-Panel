adca2c59e67c7a6284c958bc34b16bf93f402b28
d996456879 <83964663+d996456879@users.noreply.github.com>
2024-07-28 01:24:04 +0800

    _worker.js

--- _worker.js
+++ _worker.js
@@ -12,7 +12,8 @@ let userID = 'c5bb16fa-968c-4051-a7d6-75f12fe25d3f';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
-const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
+// const proxyIPs= ['cdn.xn--b6gac.eu.org', 'cdn-all.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org'];
+const proxyIPs= ['8.217.179.49'];
 
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
